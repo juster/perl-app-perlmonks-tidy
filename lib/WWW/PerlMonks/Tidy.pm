1e4f3619098fad664fa0b176eb0fc7010fbae7ea
Justin Davis <juster@mclapdawg.local>
2010-04-19 11:23:15 -0700

    Start the overhaul of WWW::PerlMonks::Tidy.
    Simplified the logic by removing the conditional load stuff for the
    CodeBlock::DB class.

--- lib/WWW/PerlMonks/Tidy.pm
+++ lib/WWW/PerlMonks/Tidy.pm
@@ -5,6 +5,8 @@ use Scalar::Util qw(refaddr);
 use English      qw(-no_match_vars);
 use Carp         qw(carp croak);
 
+use WWW::PerlMonks::Tidy::CodeBlock;
+
 {
     my %_code_class, %_dbh;
 
@@ -15,49 +17,17 @@ use Carp         qw(carp croak);
 
         my $self = bless do { my $anon_scalar; \$anon_scalar; }, $class;
 
-        # If a DBH is supplied in our argument hashref, load
-        # PerlMonks::Tidy::CodeBlock::Cached objects...
-        if ( eval { ref $params eq 'HASH' } &&
-             exists $params->{dbh} ) {
-
-            if ( ! eval { $params->{dbh}->isa('DBI::db') } ) {
-                croak qq{Invalid 'dbh' parameter supplied.\n}.
-                    qq{Must be a DBI::db object};
-            }
-
-            require PerlMonks::Tidy::CodeBlock::Cached;
-            $_dbh{refaddr $self} = $params->{dbh};
-        }
-        else {
-            require PerlMonks::Tidy::CodeBlock;
-        }
-
         *codeblock = $self->_curry_codemethod;
 
         return $self;
     }
 
-    sub _curry_codemethod
+    sub codeblock
     {
-        my ($self) = @_;
-
-        my $dbh = $_dbh{refaddr $self};
-        if ( defined $dbh ) {
-            return sub {
-                my ($self, $params) = @_;
-                if ( ! eval { ref $params eq 'HASH' } ) {
-                    croak 'Parameter must be a hashref';
-                }
-
-                my $newparams = { %{$params}, dbh => $dbh };
-                return PerlMonks::Tidy::CodeBlock::Cached->new( $newparams );
-            };
-        }
-
-        return sub {
-            return &PerlMonks::Tidy::CodeBlock::new;
-        }
+        
     }
+
+    sub 
 }
 
 1;
