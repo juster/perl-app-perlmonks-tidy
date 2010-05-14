13bd493d51ef1c7bf51c9dbe0c01a0730685351d
Justin Davis <jrcd83@gmail.com>
2010-05-14 15:42:12 -0700

    Update webtest to print request and use -g flag for GET.

--- test/webtest.pl
+++ test/webtest.pl
@@ -2,16 +2,40 @@
 
 use warnings;
 use strict;
-use LWP;
 use utf8;
+
+use HTTP::Request::Common qw(POST GET);
+use Getopt::Long          qw(GetOptions);
+use URI::Escape           qw(uri_escape);
+use LWP;
+
+my ($USE_GET);
+
+GetOptions( 'get' => \$USE_GET );
+
+die 'Provide a perl filename to send as argument'
+    unless @ARGV;
 
 my $cgiuri = 'http://juster.info/perl/pmtidy/pmtidy-1.3.pl';
 my $agent = 'PerlMonksHighlight/1.4';
 
 my $code = do { local $/; <> };
 
-my ($ua, $resp) = LWP::UserAgent->new( agent => $agent );
-$resp = $ua->post($cgiuri, Content => { 'code' => $code, 'tag' => 'P' });
+my $ua   = LWP::UserAgent->new( agent => $agent );
+my $req;
+if ( $USE_GET ) {
+    $code   = uri_escape( $code );
+    my $uri = $cgiuri . qq{?code=$code;tag=P};
+    $req    = GET $uri;
+}
+else {
+    $req = POST $cgiuri, [ 'code' => $code, 'tag' => 'P' ];
+}
+
+print "REQUEST:\n", $req->as_string, "\n", (q{-} x 70), "\n\n";
+
+my $resp = $ua->request( $req );
+print "RESPONSE:\n";
 print map { "$_ = ".$resp->header($_)."\n"; } $resp->header_field_names;
 print $resp->content;
 print "CODE: ${\$resp->code}\n";
